0901cb7e134156b391dda26094d05f1328cc9bb8
c1nderscript <c1nderscript@proton.me>
2025-07-08 21:38:47 +0000

    Fix Logger import error in test file
    - Replace non-existent Logger class import with get_logger function
    - Update test to use correct logger function API
    - This resolves the CI failure blocking all PRs
    
    Fixes: ImportError: cannot import name 'Logger' from 'src.utils.logger'

--- tests/test_file_and_logging_utils.py
+++ tests/test_file_and_logging_utils.py
@@ -1,6 +1,6 @@
 import builtins
 from src.utils.file_handler import FileHandler
-from src.utils.logger import Logger
+from src.utils.logger import get_logger
 
 
 def test_file_handler_methods_return_none(tmp_path):
@@ -9,6 +9,7 @@ def test_file_handler_methods_return_none(tmp_path):
     assert fh.write(str(tmp_path / 'file.txt'), 'data') is None
 
 
-def test_logger_log_returns_none():
-    logger = Logger()
-    assert logger.log('message') is None
+def test_logger_get_logger_returns_logger():
+    logger = get_logger('test')
+    assert logger is not None
+    assert logger.name == 'test'
