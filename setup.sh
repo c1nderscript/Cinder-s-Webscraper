4593f232958a7fa8e8a6d56acc6669a67b63c604
c1nderscript <c1nderscript@proton.me>
2025-07-11 14:43:58 +0100

    fix setup files not to truncate

--- setup.sh
+++ setup.sh
@@ -7,6 +7,11 @@ pip install -r requirements.txt
 # Prepare required directories
 mkdir -p data/logs output/scraped_data
 
-# Create empty config files if missing
-: > data/websites.json
-: > data/schedules.db
+# Create empty config files if missing without truncating existing files
+if [ ! -f "data/websites.json" ]; then
+    touch "data/websites.json"
+fi
+
+if [ ! -f "data/schedules.db" ]; then
+    touch "data/schedules.db"
+fi
